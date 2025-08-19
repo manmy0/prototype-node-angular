1b2516c6be21aacf65ea210442fada8ffe2f60be
manmy0 <105836102+manmy0@users.noreply.github.com>
2025-08-19 11:43:32 +0930

    Start making profile page with dummy data without database connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbNavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
